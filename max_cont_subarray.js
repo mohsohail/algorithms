174a3eac15d0c951746d0d5f357e9f1a529af600
mohsohail <sohail.momin1@gmail.com>
2020-03-31 18:26:05 +0530

    max contiguous subarray

---
+++ max_cont_subarray.js
@@ -0,0 +1,16 @@
+const arr = [1, 2, 3, 4, -10];
+
+const maxSubArray = A => {
+  let max_so_far = A[0];
+  let max_ending_here = A[0];
+  for (let i = 1; i < A.length; i++) {
+    max_ending_here = Math.max(max_ending_here + A[i], A[i]);
+    if (max_so_far < max_ending_here) {
+      max_so_far = max_ending_here;
+    }
+  }
+  console.log(max_so_far);
+  return max_so_far;
+};
+
+maxSubArray(arr);
