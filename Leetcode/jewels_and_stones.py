86f1ce26e2aa29431ebfc1982d481e2872e266d2
mohsohail <sohail.momin1@gmail.com>
2020-04-16 00:46:42 +0530

    jewels_and_stones

---
+++ Leetcode/jewels_and_stones.py
@@ -0,0 +1,12 @@
+J, S = "aA", "aAAbbbb"
+
+
+def func(S):
+    num_jewels = 0
+    for i in range(0, len(S)):
+        if J.find(S[i]) > -1:
+            num_jewels += 1
+    return num_jewels
+
+
+func(S)
